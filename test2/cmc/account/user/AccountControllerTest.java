ff8dc97e8d12d595a8b01b67b8f08e15d06038df
CBJORK001 <cbjork001@vplincsci053.ad.csbsju.edu>
2021-04-07 13:55:26 -0500

    Test

--- test2/cmc/account/user/AccountControllerTest.java
+++ test2/cmc/account/user/AccountControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class AccountControllerTest {
+public class AccountControllerTest extends AccountController {
 
 	@Before
 	public void setUp() throws Exception {

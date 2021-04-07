87afcb3eac8a39135c3145e0fc5804e8f36c8f29
CBJORK001 <cbjork001@vplincsci053.ad.csbsju.edu>
2021-04-07 13:47:00 -0500

    AllTests

--- test/AllTests.java
+++ test/AllTests.java
@@ -2,10 +2,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import cmc.account.user.AccountControllerTest;
 import cmc.account.user.SearchControllerTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({SearchControllerTest.class})
+@SuiteClasses({SearchControllerTest.class, AccountControllerTest.class})
 public class AllTests {
 
 }

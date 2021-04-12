b0fcde3d58b0fe1277c7fd2eed515d4f160b3264
LWOODS001 <lwoods001@vplincsci035.ad.csbsju.edu>
2021-04-11 21:56:14 -0500

    Account controller tests

--- test/cmc/account/user/AccountControllerTest.java
+++ test/cmc/account/user/AccountControllerTest.java
@@ -11,79 +11,50 @@ import cmc.account.admin.*;
 import cmc.account.user.AccountController;
 import junit.framework.Assert;
 import systemtest.*;
+import cmc.account.user.UserInteraction;
 
 public class AccountControllerTest extends AccountController {
 	private AccountController AC = new AccountController();
+	private UserInteraction UI = new UserInteraction();
 	
 	@Before
 	public void setUp() throws Exception{
+		this.UI.addUser("peter", "Peter", "Ohmann", "securepassword", 'u');
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		this.UI.deleteUser("peter");
 	}
 
-	@SuppressWarnings("deprecation")
 	@Test
-	public void testLoginMain() {
-		Account test1result = this.AC.logOn("peter", "securepassword");
-		Assert.assertTrue("successful login",test1result.getLoginStatus() == true);
-	}
-	
-	//wrong username
-
-	@SuppressWarnings("deprecation")
-	@Test
-	public void testLoginAlternate1() {
-		Account test2result = this.AC.logOn("ManMadeBeast", "securepassword");
-		Assert.assertTrue("failed login(wrong user)", test2result.getLoginStatus() == false);
-	}
-	//wrong password
-	@SuppressWarnings("deprecation")
-	@Test
-	public void testLoginAlternate2() {
-		Account test3result = this.AC.logOn("peter", "notsecurepassword");
+	public void LogOnTest() {
+		
+		//testing the main, correct scenario, good username and password
+		this.UI.userEditUser("peter", null, null, "securepassword");
+		Account test1result = this.UI.logOn("peter", "securepassword");
+		Assert.assertTrue("successful login", test1result.getLoginStatus() == true);
+		
+		//testing the scenario where an incorrect username is given
+		Account test2result = this.UI.logOn("ManMadeBeast", "securepassword");
+		Assert.assertTrue("failed login(wrong user)", test2result == null);
+		
+		//testing the scenario where an incorrect password is given
+		Account test3result = this.UI.logOn("peter", "notsecurepassword");
 		Assert.assertTrue("failed login(wrong pass)", test3result.getLoginStatus() == false);
-	}
-	//empty data
-	@SuppressWarnings("deprecation")
-	@Test
-	public void testLoginAlternate3() {
-		Account test4result = this.AC.logOn("", "");
-		Assert.assertTrue("failed login(wrong user)", test4result.getLoginStatus() == false);
+		
+		//testing the scenario where both an incorrect username and password are given
+		Account test4result = this.UI.logOn(null, null);
+		Assert.assertTrue("failed login(wrong user)", test4result == null);
+		
 	}
 	
-	@SuppressWarnings("deprecation")
-	@Test
-	public void testUserEditFirstName() {
-		Account testresult1 = this.AC.userEditUser("peter", "Charlie", "Becker", "Password1234");
-		Assert.assertEquals("Successful First Name change", "Charlie", testresult1.getFirstName());
-	}
 	
-	@SuppressWarnings("deprecation")
-	@Test
-	public void testUserEditLastName() {
-		Account testresult2 = this.AC.userEditUser("peter", "Charlie", "Becker", "Password1234");
-		Assert.assertEquals("Successful Last Name change", "Becker",testresult2.getLastName());
-	}
-	
-	@SuppressWarnings("deprecation")
-	@Test
-	public void testUserEditPassword() {
-		Account testresult3 = this.AC.userEditUser("peter", "Charlie", "Becker", "Password1234");
-		Assert.assertEquals("Successful Password change", "Password1234", testresult3.getPassWord());
-	}
-	//public void testLoginAlternate1() {
-		//boolean test2reult = AccountController.logOn("ManMade", "WRONG PASS", 'u', "Horrible", "Programmer", 'Y');
-		//Assert.assertFalse("failed login(wrong pass)", test2 result);
-	//}
 
-	
-	@SuppressWarnings("deprecation")
 	@Test
-	public void testLogOutMain() {
-		Account test5result = this.AC.logOut("peter");
-		Assert.assertEquals("successful logout", false, test5result.getLoginStatus());
+	public void LogOutTest() {
+		Account testresult = this.UI.logOut("peter");
+		Assert.assertTrue("successful logout", testresult.getLoginStatus() == false);
 	}
 
 }
